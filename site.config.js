8e8904d3b1355279c290bb0ff1dab3d72a72e847
bukita1999 <47375498+bukita1999@users.noreply.github.com>
2021-07-10 21:23:32 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -19,7 +19,7 @@ module.exports = {
   // social usernames (optional)
   twitter: '',
   github: 'bukita1999',
-  linkedin: 'bukita',
+  linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
